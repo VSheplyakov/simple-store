21bbcb3a65aa5fec9f5657bc16da8c5af9423c33
VSheplyakov <valeriy.sheplyakov@gmail.com>
2024-10-25 22:23:29 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false, path: false };
-    }
-    return config;
-  },
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.resolve.fallback = { fs: false, path: false };
+  //   }
+  //   return config;
+  // },
 };
 
 export default nextConfig;
