164a0970aeca17d8e6c6e4c176a458020a1da67d
VSheplyakov <valeriy.sheplyakov@gmail.com>
2024-10-25 23:45:42 +0300

    fix timeouts

--- app/lib/data/products.ts
+++ app/lib/data/products.ts
@@ -3,11 +3,11 @@ import sql from "better-sqlite3";
 const db = sql("phones.db");
 
 export async function getProducts() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 100));
   return db.prepare("SELECT * FROM phones").all();
 }
 
 export async function getProduct(slug: string) {
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 100));
   return db.prepare("SELECT * FROM phones WHERE slug = ?").get(slug);
 }
